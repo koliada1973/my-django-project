ee57a5737406716eb3e45ae49d8ca9dc51892fd6
Serhii Koliada <papakati1973@gmail.com>
2025-11-04 16:55:01 +0200

    Створено моделі Client, Credit, Payment. ерші налаштування адмін-панелі

--- creditsite/credit_system/admin.py
+++ creditsite/credit_system/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client, Credit, Payment
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'phone_number', 'address')
+    search_fields = ('user__username', 'phone_number')  # Пошук за логіном та телефоном
+
+admin.site.register(Client, ClientAdmin)
+
+class CreditAdmin(admin.ModelAdmin):
+    list_display = ('id', 'client', 'summa_credit', 'percent', 'start_date', 'srok_months')
+    search_fields = ('client__user__username', 'id')
+
+admin.site.register(Credit, CreditAdmin)
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('credit', 'pay', 'date_pay')
+
+admin.site.register(Payment, PaymentAdmin)
+
+
+# 2.1 Вбудована форма для додавання деталей Клієнта на сторінку User
+class ClientInlineForUser(admin.StackedInline):
+    model = Client
+    can_delete = False
+    verbose_name_plural = 'Деталі Клієнта'
+    fields = ('phone_number', 'address', 'date_of_birth',)
+
+
+# 2.2 Перевизначаємо форму User для вбудовування Client та керування правами
+admin.site.unregister(User)  # Видаляємо стандартний User
+
+
+@admin.register(User)
+class CustomUserAdmin(AuthUserAdmin):
+    inlines = (ClientInlineForUser,)
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.is_staff = False
+        super().save_model(request, obj, form, change)
--- creditsite/credit_system/models.py
+++ creditsite/credit_system/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from datetime import date
+from decimal import Decimal
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+# Модель Позичальника (Клієнта)
+# Ми використовуємо стандартного користувача Django для аутентифікації клієнтів
+from django.contrib.auth.models import User
+
+
+class Client(models.Model):
+    # User – це вбудована в Django модель, яка відповідає за аутентифікацію (вхід/вихід),
+    # безпеку (хешування паролів) і базові ідентифікаційні дані (логін, ім'я, прізвище).
+    # Зв'язок "Один до Одного" з користувачем Django - для того, щоб в подальшому клієнти могли входити на сайт під своїм логіном/паролем
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+    # Додаткові поля, яких немає в джанго-моделі User:
+    phone_number = models.CharField(max_length=20, verbose_name="Номер телефону")
+    address = models.CharField(max_length=255, verbose_name="Адреса")
+    date_of_birth = models.DateField(verbose_name="Дата народження", null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.get_full_name() or self.user.username}"
+
+    # class Meta:
+    #     verbose_name = "Клієнт"
+    #     verbose_name_plural = "Клієнти"
+
+
+class Credit(models.Model):
+    client = models.ForeignKey('Client', on_delete=models.CASCADE,related_name='credits', verbose_name="Клієнт")
+
+    summa_credit = models.DecimalField(max_digits=10, decimal_places=2,verbose_name="Основна сума кредиту")
+    percent = models.DecimalField(max_digits=5, decimal_places=3,verbose_name="Добова відсоткова ставка (%)")
+    start_date = models.DateField(verbose_name="Дата видачі")
+    srok_months = models.IntegerField(verbose_name="Термін кредиту (місяці)")
+    day_of_pay = models.IntegerField(
+        verbose_name="Плановий день оплати",
+        help_text="День місяця (від 1 до 28), коли має бути платіж.",
+        default=1,
+        validators=[MinValueValidator(1), MaxValueValidator(28)]
+    )
+
+    # class Meta:
+    #     verbose_name = "Кредит"
+    #     verbose_name_plural = "Кредити"
+
+    def __str__(self):
+        return f"Кредит №{self.id} на суму {self.summa_credit} UAH ({self.percent}% на день) для {self.client}"
+
+
+
+
+class Payment(models.Model):
+    # Зв'язок "Багато до Одного": Багато платежів до одного кредиту
+    credit = models.ForeignKey('Credit', on_delete=models.CASCADE,
+                               related_name='payments', verbose_name="Кредит")
+
+    pay = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Сума платежу")
+    date_pay = models.DateField(default=date.today, verbose_name="Дата платежу")
+
+    def __str__(self):
+        return f"Платіж {self.pay} UAH по кредиту №{self.credit.id} від {self.date_pay}"
+
+    # class Meta:
+    #     verbose_name = "Платіж"
+    #     verbose_name_plural = "Платежі"
+    #     ordering = ['-date_pay']
